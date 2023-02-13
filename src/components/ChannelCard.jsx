df9e6cfbceef6f70f7989c3ac0aed5dd6902587e
Hajima <vdhajima@gmail.com>
2023-02-13 21:59:04 +0200

    Yuotube_clone_dday2_2

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChannelCard = () => {
+    return (
+        <div>
+            ggggg
+        </div>
+    );
+}
+
+export default ChannelCard;
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,17 +1,26 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
-import {Sidebar , Videos} from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
+import { Sidebar, Videos } from './';
 
 const Feed = () => {
+    const [selectedCategory, setSelectedCategory] = useState('New');
+    const [videos, setVideos] = useState([]);
+    useEffect(() => {
+        fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+            .then((data) => setVideos(data.items))
+    },
+        [selectedCategory]);
     return (
         <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
             <Box sx={{
                 height: { sx: 'auto', md: '92vh' }
                 , borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 }
             }}>
-                <Sidebar />
+                <Sidebar
+                    selectedCategory={selectedCategory}
+                    setSelectedCategory={setSelectedCategory} />
                 <Typography
                     className="copyright"
                     variant="body2"
@@ -23,12 +32,12 @@ const Feed = () => {
                 p={2} sx={{ overflow: 'auto', height: '90vh', flex: 2 }}>
                 <Typography
                     variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
-                    New <span
+                    {selectedCategory} <span
                         style={{ color: '#F31503' }}>
                         videos
                     </span >
                 </Typography >
-                <Videos videos={{}} />
+                <Videos videos={videos} />
             </Box >
         </Stack>
     );
