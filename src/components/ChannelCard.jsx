f1c704db9530c41c7843fda81f3c7207bc67c22d
Hajima <vdhajima@gmail.com>
2023-02-18 15:50:58 +0200

    youtubeclone_dey5_1_no_err

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,35 +3,35 @@ import { CheckCircle } from '@mui/icons-material';
 import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 import { borderRadius } from '@mui/material';
-import ChannelDetail from './ChannelDetail';
+import { ChannelDetail } from './ChannelDetail';
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail , marginTop }) => {
     return (
         <Box
             sx={{
                 boxShadow: 'none',
                 borderRadius: '20px',
-                display:'flex',
-                justifyContent:'center',
-                alignItems:'center',
-                width:{xs:'356px' , md:'320px'},
-                height:'326px',
-                margin: 'auto'
-
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: { xs: '356px', md: '320px' },
+                height: '326px',
+                margin: 'auto',
+                marginTop ,
             }}>
             <Link
                 to={`/channel/${channelDetail?.id?.channelId}`}>
                 <CardContent
                     sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff' }}>
-                    <CardMedia 
-                    image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
-                    alt={channelDetail?.snippet?.title}
-                    sx={{borderRadius:'50%' , height:'180px' , width:'180px' , mb: 2 , border : '1px solid #e3e3e3'}} />
+                    <CardMedia
+                        image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture}
+                        alt={channelDetail?.snippet?.title}
+                        sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }} />
                     <Typography
-                    variant='h6' > 
-                    {channelDetail?.snippet?.title}
-                    <CheckCircle
-                     sx={{fontSize: 16 , color:'grey' , ml:'5px'}} />
+                        variant='h6' >
+                        {channelDetail?.snippet?.title}
+                        <CheckCircle
+                            sx={{ fontSize: 16, color: 'grey', ml: '5px' }} />
                     </Typography>
                     {channelDetail?.statistics?.subscriberCount && (
                         <Typography>
