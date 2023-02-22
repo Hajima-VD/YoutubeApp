8a2f810c9cb462ba043684a9162aaa7781c6c6e4
Hajima <vdhajima@gmail.com>
2023-02-22 15:35:51 +0200

    Youtubeclone

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import { NavBar, Feed , VideoDetail , ChannelDetail , SearchFeed } from "./components/";
-
+import {
+  NavBar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components/";
 
 const App = () => (
   <BrowserRouter>
-    <Box 
-    sx={{ backgroundColor: '#000' }}>
+    <Box sx={{ backgroundColor: "#000" }}>
       <NavBar />
-      
+
       <Routes>
-        <Route path="/" exact element={<Feed />}/>
+        <Route path="/" exact element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
